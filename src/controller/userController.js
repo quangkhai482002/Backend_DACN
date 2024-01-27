02c6d4da49ac70220c4211a43190d3d198dae854
quangkhai482002 <118819349+quangkhai482002@users.noreply.github.com>
2024-01-27 23:52:07 +0700

    create

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -2,23 +2,29 @@ import userAPIService from "../service/userAPIService";
 
 const readFunc = async (req, res) => {
   try {
-    if (req.query.page && req.query.limit) {
-      let page = req.query.page;
-      let limit = req.query.limit;
-      let data = await userAPIService.getUserWithPagination(+page, +limit);
-      return res.status(200).json({
-        EM: data.EM,
-        EC: data.EC,
-        DT: data.DT,
-      });
-    } else {
-      let data = await userAPIService.getAllUser();
-      return res.status(200).json({
-        EM: data.EM,
-        EC: data.EC,
-        DT: data.DT,
-      });
-    }
+    // if (req.query.page && req.query.limit) {
+    //   let page = req.query.page;
+    //   let limit = req.query.limit;
+    //   let data = await userAPIService.getUserWithPagination(+page, +limit);
+    //   return res.status(200).json({
+    //     EM: data.EM,
+    //     EC: data.EC,
+    //     DT: data.DT,
+    //   });
+    // } else {
+    //   let data = await userAPIService.getAllUser();
+    //   return res.status(200).json({
+    //     EM: data.EM,
+    //     EC: data.EC,
+    //     DT: data.DT,
+    //   });
+    // }
+    let data = await userAPIService.getAllUser();
+    return res.status(200).json({
+      EM: data.EM,
+      EC: data.EC,
+      DT: data.DT,
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({
